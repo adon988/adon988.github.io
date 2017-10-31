3195f87c6461904c6daddfc08f4c6a5297788c1e
adon988 <adon988@gmail.com>
2017-10-31 10:53:36 +0800

    test

---
+++ astro_app/sporttery_b_question.php
@@ -0,0 +1,24 @@
+	<div data-role='page' id="sporttery_b_question" >
+		<div data-role="panel" id="astropanel" data-position="right" data-theme="b"  data-display="overlay">
+			<a href="#my-header" data-rel="close"  data-prefetch="true">Close panel</a>
+		</div><!-- /panel -->
+		<div data-role='content' id="sporttery_f_history_content">
+			<div class="astro_subject ">
+				<a onclick='history.go(-1)' class="astro_back A_btn" ><img src="../../images/a01/00-02.png"/></a>
+				<span class="title">
+					<span class="ef_list_h1">
+						竟彩篮球玩法说明 
+					</span>
+				</span>
+			</div>
+			<div class="sporttery_bq_content">
+				<b>截止时间 :</b>
+				<span>
+					赛前或官网截止前2分58秒。
+				</span> 
+			</div>
+
+		</div><!--/content -->
+	</div>
+	<!--/sporttery_f_question===================================-->
+	<!--/History Page------------------------------------------------------------------>
