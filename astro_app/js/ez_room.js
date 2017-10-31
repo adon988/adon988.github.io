6ce688855d9b88b7c79ce02c4b9526988d255d02
adon988 <adon988@gmail.com>
2017-10-31 12:22:57 +0800

    addproject

--- astro_app/js/ez_room.js
+++
@@ -1,6 +0,0 @@
-var ez_line = function(){
-	$(".ez_list").each(function(){
-		var ezNum = $(this).find(".ez_num").text();
-		$(this).find(".ez_line2").animate({width: ezNum+"%"},1200);
-	})};
-	setTimeout(function(){ez_line();},600);
